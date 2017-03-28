257c53a1f2e22a67a9ee8431fe5c3a3dca359139
arnaud.zaragoza <zaragoza.arnaud@gmail.com>
2017-03-28 18:09:41 +0200

    Add a MySQL connection and update ProductController

--- src/main/java/com/valtech/tp/app1/ws/common/AppExceptionHandler.java
+++ src/main/java/com/valtech/tp/app1/ws/common/AppExceptionHandler.java
@@ -1,19 +1,26 @@
 package com.valtech.tp.app1.ws.common;
 
 import com.valtech.tp.app1.domain.model.commun.EntityAlreadyExist;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
+
 @RestControllerAdvice
 public class AppExceptionHandler {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler(Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public String processThrowable(Throwable exception) {
-        return exception.getMessage();
+    public String processThrowable(Throwable throwable) {
+        logger.error(throwable.getMessage(), throwable);
+        return throwable.getMessage();
     }
 
     @ExceptionHandler(EntityAlreadyExist.class)
@@ -21,5 +28,16 @@ public class AppExceptionHandler {
     public String processEntityAlreadyExist(EntityAlreadyExist exception) {
         return exception.getMessage();
     }
+
+    @ExceptionHandler(
+            {
+                    MethodArgumentNotValidException.class,
+                    IllegalArgumentException.class
+            })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String processNotValid(Throwable throwable) {
+        return throwable.getMessage();
+    }
+
 
 }
--- src/main/java/com/valtech/tp/app1/ws/product/ProductController.java
+++ src/main/java/com/valtech/tp/app1/ws/product/ProductController.java
@@ -1,18 +1,18 @@
 package com.valtech.tp.app1.ws.product;
 
-import com.google.common.collect.Lists;
 import com.valtech.tp.app1.domain.model.commun.EntityAlreadyExist;
 import com.valtech.tp.app1.domain.model.product.Product;
 import com.valtech.tp.app1.domain.model.product.ProductCriteria;
 import com.valtech.tp.app1.domain.model.product.ProductLite;
-import com.valtech.tp.app1.domain.repository.product.ProductRepository;
 import com.valtech.tp.app1.domain.service.product.ProductService;
 import com.valtech.tp.app1.ws.common.AbstractController;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -23,17 +23,35 @@ public class ProductController extends AbstractController {
     private ProductService productService;
 
     @GetMapping("{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable(name = "id") Long id) {
-        return new ResponseEntity<Product>(new Product(id), HttpStatus.OK);
+    public ResponseEntity<Product> getProduct(@PathVariable(name = "id") String id) {
+        return new ResponseEntity<Product>(new Product(Long.valueOf(id)), HttpStatus.OK);
     }
 
     @PostMapping
-    public ResponseEntity<Product> insertProduct(@RequestBody Product product) throws EntityAlreadyExist {
+    @ApiOperation(value = "Insert a new product")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Product created"),
+            @ApiResponse(code = 400, message = "Invalid product")
+    })
+    public ResponseEntity<Product> insertProduct(@RequestBody @Valid Product product) throws EntityAlreadyExist {
+        if(product.getId() != null) {
+            throw new IllegalArgumentException("The id has to be null");
+        }
         productService.insert(product);
         return new ResponseEntity<Product>(product, HttpStatus.CREATED);
     }
 
     @GetMapping
+    @ApiOperation(value = "Get a list of products")
+    /*
+    @ApiImplicitParams({
+            @ApiImplicitParam()
+    })
+    */
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Product created"),
+            @ApiResponse(code = 400, message = "Invalid product")
+    })
     public List<ProductLite> getProducts(@RequestParam(required = false) String name) {
         ProductCriteria productCriteria = new ProductCriteria();
         productCriteria.setName(name);
